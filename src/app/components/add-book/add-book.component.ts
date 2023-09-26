0afeb472422429088955c7efddd0263810eaacc3
SantiGO055 <santigonzalez05@gmail.com>
2023-09-26 17:50:41 -0300

    .

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-add-book',
@@ -7,4 +9,73 @@ import { Component } from '@angular/core';
 })
 export class AddBookComponent {
 
+  _imageUrl: SafeResourceUrl | undefined
+  uploaded = false
+
+  upload: String = ''
+  url: string = ''
+  constructor(private readonly storage: FirebaseService, private readonly dom: DomSanitizer) { }
+
+  set imageUrl(url: string | null) {
+    if (url) {
+      this.downloadFile(url)
+    }
+  }
+
+  ngOnInit(): void {
+    this.uploaded = false
+  }
+
+  async downloadFile(fileName: string) {
+    var retorno
+    try {
+      retorno = this.storage.referenciaCloudStorage(fileName).getDownloadURL()
+
+    } catch (error) {
+      if (error instanceof Error) {
+        // console.error('Error downloading image: ', error.message)
+      }
+    }
+    finally {
+      return retorno
+    }
+
+  }
+
+  async uploadFile(files: any) {
+
+    this.uploaded = true
+    try {
+      // console.log(files)
+
+      if (!files || files.length === 0) {
+        throw new Error('You must select an image to upload.')
+      }
+
+      const file = files[0]
+      const fileName = files[0].name
+      const fileExt = file.name.split('.').pop()
+      const filePath = `${Math.random()}.${fileExt}`
+
+
+      this.storage.tareaCloudStorage(fileName, file).then(() => {
+        this.url = this.storage.url;
+      });
+      // this.url = response
+      //this.downloadImage(fileName).then(a => a?.subscribe(response => response != null ? this.url = response : null))
+
+
+
+
+
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message)
+      }
+    } finally {
+
+
+
+    }
+  }
 }
