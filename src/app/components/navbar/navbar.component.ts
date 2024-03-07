009fabed2ad8aff281f6c7cf69493c1e701705a5
SantiGO055 <santigonzalez05@gmail.com>
2024-03-07 15:04:48 -0300

    changing navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +9,27 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  items!: MenuItem[];
+
   isLoggedIn$!: Observable<boolean>;
   isAnonymous$!: Observable<boolean>;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.items = [
+      {
+          label:'Inicio',
+          icon:'pi pi-fw pi-home',
+          routerLink: '/'
+      },
+      {
+          label:'Agregar libro',
+          icon:'pi pi-fw pi-plus-circle',
+          routerLink: '/addbook'
+      }
+  ];
+
     this.isLoggedIn$ = this.authService.isLoggedIn;
     this.isAnonymous$ = this.authService.isAnonymous;
   }
